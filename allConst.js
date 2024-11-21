761f8fdbbd2e78b878a7e304c6862fc8b2858374
Xylia-Rippy <xylia.rippy@gmail.com>
2024-11-21 14:39:46 -0800

    update

--- allConst.js
+++ allConst.js
@@ -2,16 +2,12 @@
 const { token } = require('./config.json');
 const PREFIX = '/';
 const NONE = '';
-const WEIRD = '-';
-const WEIRD2 = '+';
-const VERSION = '2.1.3';
+const VERSION = '2.1.8';
 const BOT_NAME = 'Mesa';
 
 module.exports = {
     PREFIX,
     NONE,
-    WEIRD,
-    WEIRD2,
     VERSION,
     BOT_NAME,
     token
