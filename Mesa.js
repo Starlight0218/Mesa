3bcadde702e55a781b977d6c701e52c1a5016ba6
Xylia-Rippy <xylia.rippy@gmail.com>
2024-11-21 11:18:28 -0800

    removed unessisary code

--- Mesa.js
+++ Mesa.js
@@ -63,21 +63,3 @@ client.on('message', message => {
             break;
     }
 })
-
-
-//unessisary code
-// /*
-// This is all a bet that i lost twords michael and a promis i made, i hate it, and i'll have to keep this in due to his request!
-// */
-// client.on('message', message=>{
-//     let eat = message.content.substring(weird.length).split(" ");
-//     switch(eat[0]){
-//         case 'EatMe':
-//             if(eat[1] === 'bio'){
-//                 message.channel.send('I enjoy having my flesh torn from my body with blood and bone being revealed and then watching as its consumed while asking how it tastes. I will then proceed to scream from the pain and ask you to eat the rest of me so I can die in painful bliss.');
-//             }else{
-//                 message.channel.send('Eat Me has multiple diffrent commands, Please be more specific! Also Do not do this ever again')
-//             }
-//             break;
-//     }
-// })
